f318666ac30e80e6b44824303cce0af7296943ed
Arief Pratama <ariefpratama135@gmail.com>
2023-12-24 06:32:40 +0700

    Develop (#1)
    * add shell
    
    * add sidenav
    
    * use TNT component as admin
    
    * add proxy support
    
    * add login page
    
    * add login/logout logic
    
    * add template for proxy page
    
    * add sample proxy list page

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,11 +1,37 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ToolPage from "sap/tnt/ToolPage";
+import NavContainer from "sap/m/NavContainer";
+import { logout } from "../util/Auth";
 
 /**
  * @namespace apnv.dev.controller
  */
 export default class Main extends BaseController {
-	public sayHello(): void {
-		MessageBox.show("Hello World!");
+
+	private oModel: JSONModel;
+
+	public onInit() {
+		this.oModel = new JSONModel();
+		this.oModel.loadData(sap.ui.require.toUrl("apnv/dev/model/model.json"), null, false);
+		this.getView().setModel(this.oModel);
+	}
+
+	public onItemSelect(oEvent: any) {
+		const item = oEvent.getParameter('item');
+
+		if (item.getKey() == 'logoff') {
+			logout()
+			window.location.href = '/index.html'
+		} else {
+			const container = this.byId("pageContainer") as NavContainer
+			container.to(this.getView().createId(item.getKey()));
+		}
+	}
+
+	public onMenuButtonPress() {
+		const toolPage = this.byId("toolPage") as ToolPage;
+		toolPage.setSideExpanded(!toolPage.getSideExpanded());
 	}
 }
